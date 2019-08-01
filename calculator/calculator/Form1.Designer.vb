397a5bd335388a3d04c13c96b5476670df98230d
Joe Thibedeau <jthibedeau@electratherm.com>
2019-08-01 16:55:59 -0700

    To tutorial pg 99
    basic calculator program

---
+++ calculator/calculator/Form1.Designer.vb
@@ -0,0 +1,423 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LCD = New System.Windows.Forms.TextBox()
+        Me.n1 = New System.Windows.Forms.Button()
+        Me.n2 = New System.Windows.Forms.Button()
+        Me.n3 = New System.Windows.Forms.Button()
+        Me.n4 = New System.Windows.Forms.Button()
+        Me.n5 = New System.Windows.Forms.Button()
+        Me.n6 = New System.Windows.Forms.Button()
+        Me.n7 = New System.Windows.Forms.Button()
+        Me.n8 = New System.Windows.Forms.Button()
+        Me.n9 = New System.Windows.Forms.Button()
+        Me.n0 = New System.Windows.Forms.Button()
+        Me.bc = New System.Windows.Forms.Button()
+        Me.bdiv = New System.Windows.Forms.Button()
+        Me.bad = New System.Windows.Forms.Button()
+        Me.bsub = New System.Windows.Forms.Button()
+        Me.bmult = New System.Windows.Forms.Button()
+        Me.bequal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LCD
+        '
+        Me.LCD.BackColor = System.Drawing.SystemColors.Window
+        Me.LCD.Location = New System.Drawing.Point(12, 28)
+        Me.LCD.Name = "LCD"
+        Me.LCD.ReadOnly = True
+        Me.LCD.Size = New System.Drawing.Size(470, 20)
+        Me.LCD.TabIndex = 0
+        Me.LCD.Text = "0"
+        Me.LCD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'n1
+        '
+        Me.n1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n1.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n1.Location = New System.Drawing.Point(12, 74)
+        Me.n1.Name = "n1"
+        Me.n1.Size = New System.Drawing.Size(55, 53)
+        Me.n1.TabIndex = 1
+        Me.n1.Text = "1"
+        Me.n1.UseVisualStyleBackColor = False
+        '
+        'n2
+        '
+        Me.n2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n2.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n2.Location = New System.Drawing.Point(73, 74)
+        Me.n2.Name = "n2"
+        Me.n2.Size = New System.Drawing.Size(55, 53)
+        Me.n2.TabIndex = 1
+        Me.n2.Text = "2"
+        Me.n2.UseVisualStyleBackColor = False
+        '
+        'n3
+        '
+        Me.n3.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n3.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n3.Location = New System.Drawing.Point(134, 74)
+        Me.n3.Name = "n3"
+        Me.n3.Size = New System.Drawing.Size(55, 53)
+        Me.n3.TabIndex = 1
+        Me.n3.Text = "3"
+        Me.n3.UseVisualStyleBackColor = False
+        '
+        'n4
+        '
+        Me.n4.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n4.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n4.Location = New System.Drawing.Point(12, 133)
+        Me.n4.Name = "n4"
+        Me.n4.Size = New System.Drawing.Size(55, 53)
+        Me.n4.TabIndex = 1
+        Me.n4.Text = "4"
+        Me.n4.UseVisualStyleBackColor = False
+        '
+        'n5
+        '
+        Me.n5.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n5.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n5.Location = New System.Drawing.Point(73, 133)
+        Me.n5.Name = "n5"
+        Me.n5.Size = New System.Drawing.Size(55, 53)
+        Me.n5.TabIndex = 1
+        Me.n5.Text = "5"
+        Me.n5.UseVisualStyleBackColor = False
+        '
+        'n6
+        '
+        Me.n6.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n6.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n6.Location = New System.Drawing.Point(134, 133)
+        Me.n6.Name = "n6"
+        Me.n6.Size = New System.Drawing.Size(55, 53)
+        Me.n6.TabIndex = 1
+        Me.n6.Text = "6"
+        Me.n6.UseVisualStyleBackColor = False
+        '
+        'n7
+        '
+        Me.n7.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n7.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n7.Location = New System.Drawing.Point(12, 192)
+        Me.n7.Name = "n7"
+        Me.n7.Size = New System.Drawing.Size(55, 53)
+        Me.n7.TabIndex = 1
+        Me.n7.Text = "7"
+        Me.n7.UseVisualStyleBackColor = False
+        '
+        'n8
+        '
+        Me.n8.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n8.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n8.Location = New System.Drawing.Point(73, 192)
+        Me.n8.Name = "n8"
+        Me.n8.Size = New System.Drawing.Size(55, 53)
+        Me.n8.TabIndex = 1
+        Me.n8.Text = "8"
+        Me.n8.UseVisualStyleBackColor = False
+        '
+        'n9
+        '
+        Me.n9.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n9.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n9.Location = New System.Drawing.Point(134, 192)
+        Me.n9.Name = "n9"
+        Me.n9.Size = New System.Drawing.Size(55, 53)
+        Me.n9.TabIndex = 1
+        Me.n9.Text = "9"
+        Me.n9.UseVisualStyleBackColor = False
+        '
+        'n0
+        '
+        Me.n0.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.n0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.n0.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n0.Location = New System.Drawing.Point(73, 251)
+        Me.n0.Name = "n0"
+        Me.n0.Size = New System.Drawing.Size(55, 53)
+        Me.n0.TabIndex = 1
+        Me.n0.Text = "0"
+        Me.n0.UseVisualStyleBackColor = False
+        '
+        'bc
+        '
+        Me.bc.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bc.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bc.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bc.Location = New System.Drawing.Point(134, 251)
+        Me.bc.Name = "bc"
+        Me.bc.Size = New System.Drawing.Size(55, 53)
+        Me.bc.TabIndex = 1
+        Me.bc.Text = "C"
+        Me.bc.UseVisualStyleBackColor = False
+        '
+        'bdiv
+        '
+        Me.bdiv.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bdiv.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bdiv.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bdiv.Location = New System.Drawing.Point(195, 251)
+        Me.bdiv.Name = "bdiv"
+        Me.bdiv.Size = New System.Drawing.Size(55, 53)
+        Me.bdiv.TabIndex = 1
+        Me.bdiv.Text = "/"
+        Me.bdiv.UseVisualStyleBackColor = False
+        '
+        'bad
+        '
+        Me.bad.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bad.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bad.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bad.Location = New System.Drawing.Point(195, 74)
+        Me.bad.Name = "bad"
+        Me.bad.Size = New System.Drawing.Size(55, 53)
+        Me.bad.TabIndex = 1
+        Me.bad.Text = "+"
+        Me.bad.UseVisualStyleBackColor = False
+        '
+        'bsub
+        '
+        Me.bsub.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bsub.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bsub.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bsub.Location = New System.Drawing.Point(195, 133)
+        Me.bsub.Name = "bsub"
+        Me.bsub.Size = New System.Drawing.Size(55, 53)
+        Me.bsub.TabIndex = 1
+        Me.bsub.Text = "-"
+        Me.bsub.UseVisualStyleBackColor = False
+        '
+        'bmult
+        '
+        Me.bmult.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bmult.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bmult.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bmult.Location = New System.Drawing.Point(195, 192)
+        Me.bmult.Name = "bmult"
+        Me.bmult.Size = New System.Drawing.Size(55, 53)
+        Me.bmult.TabIndex = 1
+        Me.bmult.Text = "*"
+        Me.bmult.UseVisualStyleBackColor = False
+        '
+        'bequal
+        '
+        Me.bequal.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bequal.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.bequal.Font = New System.Drawing.Font("Tahoma", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bequal.Location = New System.Drawing.Point(12, 251)
+        Me.bequal.Name = "bequal"
+        Me.bequal.Size = New System.Drawing.Size(55, 53)
+        Me.bequal.TabIndex = 1
+        Me.bequal.Text = "="
+        Me.bequal.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 450)
+        Me.Controls.Add(Me.bmult)
+        Me.Controls.Add(Me.n9)
+        Me.Controls.Add(Me.bsub)
+        Me.Controls.Add(Me.n6)
+        Me.Controls.Add(Me.bad)
+        Me.Controls.Add(Me.bdiv)
+        Me.Controls.Add(Me.n3)
+        Me.Controls.Add(Me.bc)
+        Me.Controls.Add(Me.n0)
+        Me.Controls.Add(Me.n8)
+        Me.Controls.Add(Me.n5)
+        Me.Controls.Add(Me.n2)
+        Me.Controls.Add(Me.bequal)
+        Me.Controls.Add(Me.n7)
+        Me.Controls.Add(Me.n4)
+        Me.Controls.Add(Me.n1)
+        Me.Controls.Add(Me.LCD)
+        Me.Name = "Form1"
+        Me.Text = "My Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LCD As TextBox
+    Friend WithEvents n1 As Button
+    Friend WithEvents n2 As Button
+    Friend WithEvents n3 As Button
+    Friend WithEvents n4 As Button
+    Friend WithEvents n5 As Button
+    Friend WithEvents n6 As Button
+    Friend WithEvents n7 As Button
+    Friend WithEvents n8 As Button
+    Friend WithEvents n9 As Button
+    Friend WithEvents n0 As Button
+    Friend WithEvents bc As Button
+    Friend WithEvents bdiv As Button
+    Friend WithEvents bad As Button
+    Friend WithEvents bsub As Button
+    Friend WithEvents bmult As Button
+    Friend WithEvents bequal As Button
+
+    Dim FirstNumber As Long
+    Dim Operation As String
+
+    Private Sub N1_Click(sender As Object, e As EventArgs) Handles n1.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "1"
+        Else
+            LCD.Text = LCD.Text & "1"
+        End If
+    End Sub
+
+    Private Sub N2_Click(sender As Object, e As EventArgs) Handles n2.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "2"
+        Else
+            LCD.Text = LCD.Text & "2"
+        End If
+    End Sub
+
+    Private Sub N3_Click(sender As Object, e As EventArgs) Handles n3.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "3"
+        Else
+            LCD.Text = LCD.Text & "3"
+        End If
+    End Sub
+
+    Private Sub N4_Click(sender As Object, e As EventArgs) Handles n4.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "4"
+        Else
+            LCD.Text = LCD.Text & "4"
+        End If
+    End Sub
+
+    Private Sub N5_Click(sender As Object, e As EventArgs) Handles n5.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "5"
+        Else
+            LCD.Text = LCD.Text & "5"
+        End If
+    End Sub
+
+    Private Sub N6_Click(sender As Object, e As EventArgs) Handles n6.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "6"
+        Else
+            LCD.Text = LCD.Text & "6"
+        End If
+    End Sub
+
+    Private Sub N7_Click(sender As Object, e As EventArgs) Handles n7.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "8"
+        Else
+            LCD.Text = LCD.Text & "7"
+        End If
+    End Sub
+
+    Private Sub N8_Click(sender As Object, e As EventArgs) Handles n8.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "8"
+        Else
+            LCD.Text = LCD.Text & "8"
+        End If
+    End Sub
+
+    Private Sub N9_Click(sender As Object, e As EventArgs) Handles n9.Click
+        If LCD.Text = "0" Then
+            LCD.Text = "9"
+        Else
+            LCD.Text = LCD.Text & "9"
+        End If
+    End Sub
+
+    Private Sub N0_Click(sender As Object, e As EventArgs) Handles n0.Click
+        If LCD.Text <> "0" Then
+            LCD.Text = LCD.Text & "0"
+        End If
+    End Sub
+
+    Private Sub BC_Click(sender As Object, e As EventArgs) Handles bc.Click
+        LCD.Text = "0"
+    End Sub
+
+    Private Sub Bad_Click(sender As Object, e As EventArgs) Handles bad.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "+"
+    End Sub
+
+    Private Sub Bsub_Click(sender As Object, e As EventArgs) Handles bsub.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "-"
+    End Sub
+
+    Private Sub Bmult_Click(sender As Object, e As EventArgs) Handles bmult.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "*"
+    End Sub
+
+    Private Sub Bdiv_Click(sender As Object, e As EventArgs) Handles bdiv.Click
+        FirstNumber = LCD.Text
+        LCD.Text = "0"
+        Operation = "/"
+    End Sub
+
+    Private Sub Bequal_Click(sender As Object, e As EventArgs) Handles bequal.Click
+        Dim SecondNumber As Long
+        Dim Result As Long
+
+        SecondNumber = LCD.Text
+
+        If Operation = "+" Then
+            Result = FirstNumber + SecondNumber
+        ElseIf Operation = "-" Then
+            Result = FirstNumber - SecondNumber
+        ElseIf Operation = "*" Then
+            Result = FirstNumber * SecondNumber
+        ElseIf Operation = "/" Then
+            Result = FirstNumber / SecondNumber
+        End If
+
+        FirstNumber = Result
+        LCD.Text = Result
+
+    End Sub
+End Class
